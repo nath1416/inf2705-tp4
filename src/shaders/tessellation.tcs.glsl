e78343135fe8640c385211eb815d6f35fd29853c
elborak3 <elborak3@gmail.com>
2024-11-27 14:02:52 -0500

    le tcs est fait (normalement)

--- src/shaders/tessellation.tcs.glsl
+++ src/shaders/tessellation.tcs.glsl
@@ -23,17 +23,27 @@ void main()
     vec4 vVec = p10 - p00;
     vec4 normal = normalize( vec4(cross(vVec.xyz, uVec.xyz), 0) );
 
-    float dist = length(modelView[3] - p00);
+    float dist1 = length(modelView[3] - p00);
+    float dist2 = length(modelView[3] - p01);
+    float dist3 = length(modelView[3] - p10);
+    float dist4 = length(modelView[3] - p11);
 
-    float mixFactor = clamp( (dist - MIN_DIST) / (MAX_DIST - MIN_DIST), 0.0f, 1.0f );
-    mixFactor = mix(MIN_TESS, MAX_TESS, mixFactor);
+    float mixFactor1 = clamp( (dist1 - MIN_DIST) / (MAX_DIST - MIN_DIST), 0.0f, 1.0f );
+    float mixFactor2 = clamp( (dist2 - MIN_DIST) / (MAX_DIST - MIN_DIST), 0.0f, 1.0f );
+    float mixFactor3 = clamp( (dist3 - MIN_DIST) / (MAX_DIST - MIN_DIST), 0.0f, 1.0f );
+    float mixFactor4 = clamp( (dist4 - MIN_DIST) / (MAX_DIST - MIN_DIST), 0.0f, 1.0f );
 
-    gl_TessLevelOuter[0] = mixFactor;
-    gl_TessLevelOuter[1] = mixFactor;
-    gl_TessLevelOuter[2] = mixFactor;
-    gl_TessLevelOuter[3] = mixFactor;
-    gl_TessLevelInner[0] = mixFactor;
-    gl_TessLevelInner[1] = mixFactor;
+    float resultFactor1 = mix(MIN_TESS, MAX_TESS, mixFactor1);
+    float resultFactor2 = mix(MIN_TESS, MAX_TESS, mixFactor2);
+    float resultFactor3 = mix(MIN_TESS, MAX_TESS, mixFactor3);
+    float resultFactor4 = mix(MIN_TESS, MAX_TESS, mixFactor4);
+
+    gl_TessLevelOuter[0] = resultFactor1;
+    gl_TessLevelOuter[1] = resultFactor2;
+    gl_TessLevelOuter[2] = resultFactor3;
+    gl_TessLevelOuter[3] = resultFactor4;
+    gl_TessLevelInner[0] = max(resultFactor1, resultFactor3);
+    gl_TessLevelInner[1] = max(resultFactor2, resultFactor4);
 
     gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
 }
