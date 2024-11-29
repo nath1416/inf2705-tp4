9ed8b9bbfcc99a39279a0c529bdabecc197d0a8f
Nath1416 <bougienath@gmail.com>
2024-11-29 14:28:57 -0500

    Enlver code mort

--- src/shaders/tessellation.gs.glsl
+++ src/shaders/tessellation.gs.glsl
@@ -17,10 +17,6 @@ out ATTRIB_GS_OUT {
 } attribOut;
 
 void main() {
-    vec3 bc0 = vec3(1.0, 0.0, 0.0);
-    vec3 bc1 = vec3(0.0, 1.0, 0.0);
-    vec3 bc2 = vec3(0.0, 0.0, 1.0);
-
     for (int i = 0; i < gl_in.length(); ++i) {
         attribOut.height = attribIn[i].height;
         attribOut.texCoords = attribIn[i].texCoords;
